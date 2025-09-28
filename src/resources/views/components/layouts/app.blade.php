f4637fb0b6c56f1d3ba8da21b6f8523ffe1e518b
jisungyoo <jisung87kr@gmail.com>
2025-09-28 19:51:39 +0900

    ga4 설치

--- src/resources/views/components/layouts/app.blade.php
+++ src/resources/views/components/layouts/app.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="ko">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PJV2CH8GCP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-PJV2CH8GCP');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
